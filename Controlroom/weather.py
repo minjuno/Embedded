e51d93e25c0e3a583916935f6e63e04e0e88f397
minjuno <128601394+minjuno@users.noreply.github.com>
2024-06-11 01:02:00 +0900

    Update weather.py

--- Controlroom/weather.py
+++ Controlroom/weather.py
@@ -1,6 +1,6 @@
 import requests
 
-API_KEY = 'e65645856d2f9f8a5fb3206da89f2305'
+API_KEY = '{your api keys}'
 
 def get_weather(city):
     weather_url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric'
